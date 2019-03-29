7d43d84ff167b75270e36194d388cbbba86d36ed
ivanightingale <guancheq@usc.edu>
2019-03-28 20:02:54 -0700

    Update database.sql

--- imhungry/src/main/webapp/resources/database.sql
+++ imhungry/src/main/webapp/resources/database.sql
@@ -3,45 +3,45 @@ CREATE DATABASE ImHungryDatabase;
 USE ImHungryDatabase;
              
 CREATE TABLE Restaurants (
-    restaurantKeyID INT(11) PRIMARY KEY AUTO_INCREMENT,
-    restaurantName VARCHAR(200) NOT NULL,
-    restaurantRating INT(11) NOT NULL,
-    placeID VARCHAR(200) NOT NULL,
-    address VARCHAR(200) NOT NULL,
-    price INT(11) NOT NULL,  -- note that the constructor takes an integer for price level
-    driveTimeText VARCHAR(200) NOT NULL,
-    driveTimeValue INT(11) NOT NULL,
-    phone VARCHAR(200) NOT NULL,
-    website VARCHAR(200) NOT NULL,
-    restaurantInList VARCHAR(20) NOT NULL  -- 'favorite', 'toexplore', 'donotshow'
+    RestaurantKeyID INT(11) PRIMARY KEY AUTO_INCREMENT,
+    RestaurantName VARCHAR(200) NOT NULL,
+    RestaurantRating INT(11) NOT NULL,
+    PlaceID VARCHAR(200) NOT NULL,
+    Address VARCHAR(200) NOT NULL,
+    Price INT(11) NOT NULL,  -- note that the constructor takes an integer for price level
+    DriveTimeText VARCHAR(200) NOT NULL,
+    DriveTimeValue INT(11) NOT NULL,
+    Phone VARCHAR(200) NOT NULL,
+    Website VARCHAR(200) NOT NULL,
+    RestaurantInList VARCHAR(20) NOT NULL  -- 'favorite', 'toexplore', 'donotshow'
 );
 
 CREATE TABLE Recipes(
-    recipeKeyID INT(11) PRIMARY KEY AUTO_INCREMENT,
-    recipetName VARCHAR(200) NOT NULL,
-    recipeRating INT(11) NOT NULL,
-    recipeID INT(11) NOT NULL,
-    prepTime INT(11) NOT NULL,
-    cookTime INT(11) NOT NULL,
-    imageURL VARCHAR(500) NOT NULL,
-    recipeInList VARCHAR(20) NOT NULL  -- 'favorite', 'toexplore', 'donotshow'
+    RecipeKeyID INT(11) PRIMARY KEY AUTO_INCREMENT,
+    RecipeName VARCHAR(200) NOT NULL,
+    RecipeRating INT(11) NOT NULL,
+    RecipeID INT(11) NOT NULL,
+    PrepTime INT(11) NOT NULL,
+    CookTime INT(11) NOT NULL,
+    ImageURL VARCHAR(500) NOT NULL,
+    RecipeInList VARCHAR(20) NOT NULL  -- 'favorite', 'toexplore', 'donotshow'
 );
 
 CREATE TABLE Ingredients (
-    ingredientID INT(11) PRIMARY KEY AUTO_INCREMENT,
-    recipeKeyID INT(11) NOT NULL,  -- SELECT LAST_INSERT_ID();
-    ingredient VARCHAR(200) NOT NULL,
-    FOREIGN KEY (recipeKeyID) REFERENCES Recipes(recipeKeyID)
+    IngredientID INT(11) PRIMARY KEY AUTO_INCREMENT,
+    RecipeKeyID INT(11) NOT NULL,  -- use 'SELECT LAST_INSERT_ID();' to get RecipeKeyID
+    Ingredient VARCHAR(200) NOT NULL,
+    FOREIGN KEY (RecipeKeyID) REFERENCES Recipes(recipeKeyID)
 );
 
 CREATE TABLE Instructions(
-    instructionID INT(11) PRIMARY KEY AUTO_INCREMENT,
-    recipeKeyID INT(11) NOT NULL,  -- SELECT LAST_INSERT_ID();
-    instruction VARCHAR(200) NOT NULL,
-    FOREIGN KEY (recipeKeyID) REFERENCES Recipes(recipeKeyID)
+    InstructionID INT(11) PRIMARY KEY AUTO_INCREMENT,
+    RecipeKeyID INT(11) NOT NULL,
+    Instruction VARCHAR(200) NOT NULL,
+    FOREIGN KEY (RecipeKeyID) REFERENCES Recipes(recipeKeyID)
 );
 
-CREATE TABLE Grocery(
-    groceryID INT(11) PRIMARY KEY AUTO_INCREMENT,
-    groceryItem VARCHAR(200) NOT NULL
+CREATE TABLE GroceryList(
+    GroceryID INT(11) PRIMARY KEY AUTO_INCREMENT,
+    GroceryItem VARCHAR(200) NOT NULL
 );
