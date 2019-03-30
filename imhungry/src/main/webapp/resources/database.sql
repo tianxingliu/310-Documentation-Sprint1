b10af4f35d0c091c3fbbd3e6f68e1afad189213c
ivanightingale <guancheq@usc.edu>
2019-03-29 17:50:25 -0700

    Update database.sql

--- imhungry/src/main/webapp/resources/database.sql
+++ imhungry/src/main/webapp/resources/database.sql
@@ -3,10 +3,9 @@ CREATE DATABASE ImHungryDatabase;
 USE ImHungryDatabase;
              
 CREATE TABLE Restaurants (
-    RestaurantKeyID INT(11) PRIMARY KEY AUTO_INCREMENT,
+    PlaceID VARCHAR(200) PRIMARY KEY,
     RestaurantName VARCHAR(200) NOT NULL,
     RestaurantRating INT(11) NOT NULL,
-    PlaceID VARCHAR(200) NOT NULL,
     Address VARCHAR(200) NOT NULL,
     Price INT(11) NOT NULL,  -- note that the constructor takes an integer for price level
     DriveTimeText VARCHAR(200) NOT NULL,
@@ -19,10 +18,9 @@ CREATE TABLE Restaurants (
 );
 
 CREATE TABLE Recipes(
-    RecipeKeyID INT(11) PRIMARY KEY AUTO_INCREMENT,
+    RecipeID INT(11) PRIMARY KEY,
     RecipeName VARCHAR(200) NOT NULL,
     RecipeRating INT(11) NOT NULL,
-    RecipeID INT(11) NOT NULL,
     PrepTime INT(11) NOT NULL,
     CookTime INT(11) NOT NULL,
     Ingredients VARCHAR(500) NOT NULL,
