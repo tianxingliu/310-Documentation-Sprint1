5171e3bdf66213d69e10b9e9571e71f184513bcd
Minxuan Song <aidenixus@Minxuans-MacBook-Pro.local>
2019-03-31 16:08:44 -0700

    Test added

---
+++ imhungry/src/test/java/info/SearchResultTest.java
@@ -0,0 +1,22 @@
+package info;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class SearchResultTest {
+
+	@Test
+	//testing with a random string
+	public void twoParamInstantiation1() { 
+		List<List<Info>> results = new ArrayList<List<Info>>();
+		List<String> imageURLs = new ArrayList<String>();
+		SearchResult sr = new SearchResult(results,imageURLs);
+		assertEquals(results,sr.results);
+		assertEquals(imageURLs,sr.imageURLs);
+	}
+
+}
