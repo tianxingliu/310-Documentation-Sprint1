a740e71216c74093ec1432e70ad2158a1cb85e48
Robinlph <pinghao@qvsta.com>
2019-04-25 19:47:07 -0700

    yeah

--- imhungry/src/main/webapp/resources/database.sql
+++ imhungry/src/main/webapp/resources/database.sql
@@ -4,7 +4,7 @@ USE ImHungryDatabase;
 
 CREATE TABLE Users(
     Username VARCHAR(50) PRIMARY KEY,
-    Password VARCHAR(50) NOT NULL
+    Password VARCHAR(500) NOT NULL
 );
 
 INSERT INTO Users VALUES('nero', 'domusaurea');
