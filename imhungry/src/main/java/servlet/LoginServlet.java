cc4da5131280763009db3d25ffebf167f51f0480
ivanightingale <guancheq@usc.edu>
2019-04-26 01:21:59 -0700

    tests

--- imhungry/src/main/java/servlet/LoginServlet.java
+++ imhungry/src/main/java/servlet/LoginServlet.java
@@ -10,10 +10,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import database_manager.UserDataManager;
-import info.*;
 
 
 
